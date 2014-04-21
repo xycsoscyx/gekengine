20c6cca0115af303d7ad5f43e1924558f06ecb9f
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-04-21 04:24:04 +0000

    Adding basic collision api

--- api/IGEKModelManager.h
+++ api/IGEKModelManager.h
@@ -5,7 +5,11 @@
 
 DECLARE_INTERFACE_IID_(IGEKModelManager, IUnknown, "E44090AA-D287-444A-AACF-399015AB0D18")
 {
-    STDMETHOD(LoadModel)        (THIS_ LPCWSTR pName, LPCWSTR pParams, IUnknown **ppModel) PURE;
+    STDMETHOD(LoadCloud)            (THIS_ LPCWSTR pName, LPCWSTR pParams, IUnknown **ppCloud) PURE;
+
+    STDMETHOD(LoadCollision)        (THIS_ LPCWSTR pName, LPCWSTR pParams, IUnknown **ppTriangles) PURE;
+
+    STDMETHOD(LoadModel)            (THIS_ LPCWSTR pName, LPCWSTR pParams, IUnknown **ppModel) PURE;
 };
 
 SYSTEM_USER(ModelManager, "EE07DA62-6E01-4EF0-ADA3-07631DB6B4A4");
