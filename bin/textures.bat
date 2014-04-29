e5432e1c09f3ffc43115714eba7ba7afd7c7053d
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-04-29 23:34:52 +0000

    Trying to normalize material values

--- bin/textures.bat
+++ bin/textures.bat
@@ -1,9 +1,11 @@
 echo off
 For /R "data/textures" %%# in (*.png,*.tga) Do (
     Echo %%~nx# | FIND /I "colormap" 1>NUL && (
+        del "%%~dpn#.dds"
         nvdxt -file "%%~#" -output "%%~dpn#.dds" -quality_production -dxt5
     )
     Echo %%~nx# | FIND /I "normalmap" 1>NUL && (
+        del "%%~dpn#.dds"
         nvdxt -file "%%~#" -output "%%~dpn#.dds" -quality_production -3Dc
     )
 )
