f4715f68f9000796bb4faaed2421a6188a0586b2
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-05-05 06:26:44 +0000

    Removing alpha to coverage in lieu of alpha testing

--- plugins/engine/Source/CGEKProperties.cpp
+++ plugins/engine/Source/CGEKProperties.cpp
@@ -147,13 +147,6 @@ void GetTargetStates(GEKVIDEO::TARGETBLENDSTATES &kStates, CLibXMLNode &kNode)
 
 HRESULT CGEKBlendStates::Load(IGEKVideoSystem *pSystem, CLibXMLNode &kBlend)
 {
-    bool bAlphaToCoverage = false;
-/*
-    if (kBlend.HasAttribute(L"alphatocoverage") && StrToBoolean(kBlend.GetAttribute(L"alphatocoverage")))
-    {
-        bAlphaToCoverage = true;
-    }
-*/
     if (kBlend.HasAttribute(L"factor"))
     {
         m_nBlendFactor = StrToFloat4(kBlend.GetAttribute(L"factor"));
@@ -171,7 +164,6 @@ HRESULT CGEKBlendStates::Load(IGEKVideoSystem *pSystem, CLibXMLNode &kBlend)
             kTarget = kTarget.NextSiblingElement(L"target");
         };
 
-        kBlendStates.m_bAlphaToCoverage = bAlphaToCoverage;
         hRetVal = pSystem->CreateBlendStates(kBlendStates, &m_spBlendStates);
 
     }
@@ -179,7 +171,6 @@ HRESULT CGEKBlendStates::Load(IGEKVideoSystem *pSystem, CLibXMLNode &kBlend)
     {
         GEKVIDEO::UNIFIEDBLENDSTATES kBlendStates;
         GetTargetStates(kBlendStates, kBlend);
-        kBlendStates.m_bAlphaToCoverage = bAlphaToCoverage;
         hRetVal = pSystem->CreateBlendStates(kBlendStates, &m_spBlendStates);
     }
 
