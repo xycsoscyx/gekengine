dba825bee256a9e2b7d0dceb0215bfa37b043d43
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-05-30 00:17:43 +0000

    Adding simple resource manager with threaded support

---
+++ plugins/engine/Include/CGEKResourceManager.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "GEKContext.h"
+#include "GEKSystem.h"
+#include <concurrent_unordered_map.h>
+#include <concurrent_queue.h>
+#include <thread>
+
+class CGEKResourceManager : public CGEKUnknown
+{
+public:
+    struct REQUEST
+    {
+        CStringW m_strName;
+        CStringW m_strParams;
+        REQUEST(void)
+        {
+        }
+
+        REQUEST(LPCWSTR pName, LPCWSTR pParams)
+            : m_strName(pName)
+            , m_strParams(pParams)
+        {
+        }
+    };
+
+    struct RESOURCE
+    {
+        UINT32 m_nSession;
+        CComPtr<IUnknown> m_spObject;
+    };
+
+private:
+    IGEKSystem *m_pSystem;
+    IGEKVideoSystem *m_pVideoSystem;
+
+    UINT32 m_nSession;
+    concurrency::critical_section m_kCritical;
+    concurrency::concurrent_queue<REQUEST> m_aQueue;
+    concurrency::concurrent_unordered_multimap<GEKHASH, std::function<void(IUnknown *)>> m_aCallbacks;
+    std::map<GEKHASH, CComPtr<IUnknown>> m_aResources;
+    std::unique_ptr<std::thread> m_spThread;
+
+public:
+    CGEKResourceManager(void);
+    virtual ~CGEKResourceManager(void);
+    DECLARE_UNKNOWN(CGEKResourceManager);
+
+    // IGEKUnknown
+    STDMETHOD(Initialize)                   (THIS);
+    STDMETHOD_(void, Destroy)               (THIS);
+
+    // IGEKResourceManager
+    STDMETHOD(Load)                         (THIS_ LPCWSTR pName, LPCWSTR pParams, std::function<void(IUnknown *)> OnReady);
+};
---
+++ plugins/engine/Source/CGEKResourceManager.cpp
@@ -0,0 +1,84 @@
+﻿#include "CGEKResourceManager.h"
+
+#include "GEKEngineCLSIDs.h"
+#include "GEKSystemCLSIDs.h"
+
+BEGIN_INTERFACE_LIST(CGEKResourceManager)
+END_INTERFACE_LIST_UNKNOWN
+
+REGISTER_CLASS(CGEKResourceManager)
+
+CGEKResourceManager::CGEKResourceManager(void)
+    : m_pSystem(nullptr)
+    , m_pVideoSystem(nullptr)
+{
+}
+
+CGEKResourceManager::~CGEKResourceManager(void)
+{
+}
+
+STDMETHODIMP CGEKResourceManager::Initialize(void)
+{
+    GEKFUNCTION(nullptr);
+    HRESULT hRetVal = E_FAIL;
+    m_pSystem = GetContext()->GetCachedClass<IGEKSystem>(CLSID_GEKSystem);
+    m_pVideoSystem = GetContext()->GetCachedClass<IGEKVideoSystem>(CLSID_GEKVideoSystem);
+    if (m_pSystem != nullptr && m_pVideoSystem != nullptr)
+    {
+        m_spThread.reset(new std::thread([&](void)-> void
+        {
+            while (true)
+            {
+                REQUEST kRequest;
+                if (m_aQueue.try_pop(kRequest))
+                {
+                    concurrency::critical_section::scoped_lock kLock(m_kCritical);
+
+                    GEKHASH nID(kRequest.m_strName + L"|" + kRequest.m_strParams);
+
+                    m_aResources[nID] = nullptr;
+
+                    auto pRange = m_aCallbacks.equal_range(nID);
+                    auto pIterator = pRange.first;
+                    while(pIterator != pRange.second)
+                    {
+                        (*pIterator).second(nullptr);
+                        m_aCallbacks.unsafe_erase(++pIterator);
+                    };
+                }
+            };
+        }));
+
+        hRetVal = S_OK;
+    }
+
+    return hRetVal;
+}
+
+STDMETHODIMP_(void) CGEKResourceManager::Destroy(void)
+{
+}
+
+STDMETHODIMP CGEKResourceManager::Load(LPCWSTR pName, LPCWSTR pParams, std::function<void(IUnknown *)> OnReady)
+{
+    concurrency::critical_section::scoped_lock kLock(m_kCritical);
+
+    GEKHASH nID = FormatString(L"%s|%s", pName, pParams);
+    auto pIterator = m_aResources.find(nID);
+    if (pIterator != m_aResources.end())
+    {
+        OnReady((*pIterator).second);
+    }
+    else
+    {
+        if (m_aCallbacks.find(nID) == m_aCallbacks.end())
+        {
+            m_aQueue.push(REQUEST(pName, pParams));
+        }
+
+        m_aCallbacks.insert(std::make_pair(nID, OnReady));
+    }
+
+    return S_OK;
+}
