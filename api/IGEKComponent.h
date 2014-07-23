dcbeb7299fdca4ed243b21765bf461ad1e031e35
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-07-23 01:26:48 +0000

    First step towards a better tomorrow

--- api/IGEKComponent.h
+++ api/IGEKComponent.h
@@ -2,38 +2,16 @@
 
 #include "GEKUtility.h"
 #include "GEKVALUE.h"
-#include <concurrent_unordered_set.h>
-
-DECLARE_INTERFACE(IGEKEntity);
+#include "IGEKSceneManager.h"
 
 DECLARE_INTERFACE_IID_(IGEKComponent, IUnknown, "F1CA9EEC-0F09-45DA-BF24-0C70F5F96E3E")
 {
-    STDMETHOD_(IGEKEntity *, GetEntity)         (THIS) const PURE;
-
-    STDMETHOD_(void, ListProperties)            (THIS_ std::function<void(LPCWSTR, const GEKVALUE &)> OnProperty) PURE;
-    STDMETHOD_(bool, GetProperty)               (THIS_ LPCWSTR pName, GEKVALUE &kValue) const PURE;
-    STDMETHOD_(bool, SetProperty)               (THIS_ LPCWSTR pName, const GEKVALUE &kValue) PURE;
-
-    STDMETHOD(OnEntityCreated)                  (THIS) { return S_OK; }
-    STDMETHOD_(void, OnEntityDestroyed)         (THIS) { }
-    STDMETHOD_(void, OnEvent)                   (THIS_ LPCWSTR pAction, const GEKVALUE &kParamA, const GEKVALUE &kParamB) { };
+    STDMETHOD_(void, ListProperties)            (THIS_ const GEKENTITYID &nEntityID, std::function<void(LPCWSTR, const GEKVALUE &)> OnProperty) PURE;
+    STDMETHOD_(bool, GetProperty)               (THIS_ const GEKENTITYID &nEntityID, LPCWSTR pName, GEKVALUE &kValue) const PURE;
+    STDMETHOD_(bool, SetProperty)               (THIS_ const GEKENTITYID &nEntityID, LPCWSTR pName, const GEKVALUE &kValue) PURE;
 };
 
-DECLARE_INTERFACE_IID_(IGEKEntity, IUnknown, "30A6AF4F-AD6C-4EFB-A239-11532E8B983F")
+DECLARE_INTERFACE_IID_(IGEKSystem, IUnknown, "81A24012-F085-42D0-B931-902485673E90")
 {
-    STDMETHOD_(void, ListComponents)            (THIS_ std::function<void(IGEKComponent *)> OnComponent) PURE;
-    STDMETHOD_(IGEKComponent *, GetComponent)   (THIS_ LPCWSTR pName) PURE;
-
-    STDMETHOD_(void, OnEvent)                   (THIS_ LPCWSTR pAction, const GEKVALUE &kParamA = GEKVALUE(), const GEKVALUE &kParamB = GEKVALUE()) PURE;
-};
-
-DECLARE_INTERFACE_IID_(IGEKComponentSystem, IUnknown, "81A24012-F085-42D0-B931-902485673E90")
-{
-    STDMETHOD_(LPCWSTR, GetType)                (THIS) const PURE;
-
-    STDMETHOD(Create)                           (THIS_ const CLibXMLNode &kEntityNode, IGEKEntity *pEntity, IGEKComponent **ppComponent) PURE;
-    STDMETHOD(Destroy)                          (THIS_ IGEKEntity *pEntity) PURE;
-
-    STDMETHOD_(void, GetVisible)                (THIS_ const frustum &kFrustum, concurrency::concurrent_unordered_set<IGEKEntity *> &aVisibleEntities) { };
 };
 
--- api/IGEKSceneManager.h
+++ api/IGEKSceneManager.h
@@ -2,17 +2,24 @@
 
 #include "GEKContext.h"
 
+DECLARE_INTERFACE(IGEKComponent);
+
+typedef UINT32 GEKENTITYID;
+
 DECLARE_INTERFACE_IID_(IGEKSceneManager, IUnknown, "43DF2FD7-3BE2-4333-86ED-CB1221C6599B")
 {
-    STDMETHOD(Load)                     (LPCWSTR pName, LPCWSTR pEntry) PURE;
+    STDMETHOD(Load)                             (THIS_ LPCWSTR pName, LPCWSTR pEntry) PURE;
 
-    STDMETHOD(AddEntity)                (THIS_ CLibXMLNode &kEntityNode, IGEKEntity **ppEntity = nullptr) PURE;
-    STDMETHOD(FindEntity)               (THIS_ LPCWSTR pName, IGEKEntity **ppEntity) PURE;
-    STDMETHOD(DestroyEntity)            (THIS_ IGEKEntity *pEntity) PURE;
+    STDMETHOD_(float3, GetGravity)              (THIS_ const float3 &nPosition) PURE;
 
-    STDMETHOD_(float3, GetGravity)      (THIS_ const float3 &nPosition) PURE;
+    STDMETHOD(CreateEntity)                     (THIS_ GEKENTITYID &nEntityID) PURE;
+    STDMETHOD(DestroyEntity)                    (THIS_ const GEKENTITYID &nEntityID) PURE;
 
-    STDMETHOD_(void, GetVisible)        (THIS_ const frustum &kFrustum, concurrency::concurrent_unordered_set<IGEKEntity *> &aVisibleEntities) PURE;
+    STDMETHOD(AddComponent)                     (THIS_ const GEKENTITYID &nEntityID, LPCWSTR pComponent) PURE;
+    STDMETHOD(RemoveComponent)                  (THIS_ const GEKENTITYID &nEntityID, LPCWSTR pComponent) PURE;
+    STDMETHOD_(bool, HasComponent)              (THIS_ const GEKENTITYID &nEntityID, LPCWSTR pComponent) PURE;
+
+    STDMETHOD_(IGEKComponent *, GetComponent)   (THIS_ LPCWSTR pComponent) PURE;
 };
 
 DECLARE_INTERFACE_IID_(IGEKSceneObserver, IGEKObserver, "51D6E5E6-2AD3-4D61-A704-8E6515F024F9")
@@ -21,9 +28,6 @@ DECLARE_INTERFACE_IID_(IGEKSceneObserver, IGEKObserver, "51D6E5E6-2AD3-4D61-A704
     STDMETHOD(OnLoadEnd)                (THIS_ HRESULT hRetVal) { return S_OK; };
 
     STDMETHOD_(void, OnFree)            (THIS) { };
-
-    STDMETHOD_(void, OnEntityAdded)     (THIS_ IGEKEntity *pEntity) { };
-    STDMETHOD_(void, OnEntityDestroyed) (THIS_ IGEKEntity *pEntity) { };
 
     STDMETHOD_(void, OnPreUpdate)       (THIS_ float nGameTime, float nFrameTime) { };
     STDMETHOD_(void, OnUpdate)          (THIS_ float nGameTime, float nFrameTime) { };
