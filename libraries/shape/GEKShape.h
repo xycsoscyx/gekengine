eef90663a601dcda95712968ff0dc858c128eb85
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-09-19 06:06:21 +0000

    Holding off on moving to Eigen for now

--- libraries/shape/GEKShape.h
+++ libraries/shape/GEKShape.h
@@ -2,12 +2,16 @@
 
 #include "GEKMath.h"
 
-#include <Eigen\Core>
-#include <Eigen\Geometry>
+#include "Public\Plane.h"
+#include "Public\Sphere.h"
+#include "Public\AABB.h"
+#include "Public\OBB.h"
+#include "Public\Ray.h"
+#include "Public\Frustum.h"
 
-#include "Include\GEKPlane.h"
-#include "Include\GEKSphere.h"
-#include "Include\GEKAABB.h"
-#include "Include\GEKOBB.h"
-#include "Include\GEKRay.h"
-#include "Include\GEKFrustum.h"
+typedef tplane<float> plane;
+typedef tsphere<float> sphere;
+typedef taabb<float> aabb;
+typedef tobb<float> obb;
+typedef tray<float> ray;
+typedef tfrustum<float> frustum;
