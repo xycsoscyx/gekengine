c208e83cde5cbb5f33016f66084f9cb5db675a1e
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2014-12-02 07:38:28 +0000

    Using 0 alpha by default for reflectivity info

--- bin/addalpha.bat
+++ bin/addalpha.bat
@@ -1,6 +1,6 @@
 For /R "data/textures" %%# in (*.png,*.tga,*.jpg) Do (
     Echo %%~nx# | FIND /I "infomap" 1>NUL && (
-		convert "%%~#" -alpha set -channel A -evaluate set 100%% "%%~#"
+		convert "%%~#" -alpha set -channel A -evaluate set 0 "%%~#"
 	)
 )
 
