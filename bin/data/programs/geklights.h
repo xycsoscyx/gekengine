f6803182e75a412b0fdbd09b5aee0ac2cca09f3c
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-03-18 00:09:13 +0000

    Fixing lighting, need better synchronization between compute and pixel program buffers

--- bin/data/programs/geklights.h
+++ bin/data/programs/geklights.h
@@ -6,7 +6,11 @@ struct LIGHT
     float   m_nInvRange;
 };
 
+#ifdef _TYPE_COMPUTE
+cbuffer LIGHTBUFFER                     : register(b1)
+#else
 cbuffer LIGHTBUFFER                     : register(b2)
+#endif
 {
     uint    gs_nNumLights               : packoffset(c0);
     uint3   gs_nLightPadding            : packoffset(c0.y);
