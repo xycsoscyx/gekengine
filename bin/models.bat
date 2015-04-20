6d95166bfb810a678afbe18b6974a4affee773b5
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-04-20 13:33:29 +0000

    Adding base tiles

--- bin/models.bat
+++ bin/models.bat
@@ -1,4 +1,11 @@
 .\debug\optimizer.debug -input data\models\sponza.dae -output data\models\sponza.gek -smooth 75
+
 .\debug\optimizer.debug -input data\models\boulder.dae -output data\models\boulder.gek -smooth 75
+
+.\debug\optimizer.debug -input data\models\center.dae -output data\models\center.gek -smooth 75
+.\debug\optimizer.debug -input data\models\tl.dae -output data\models\tl.gek -smooth 75
+.\debug\optimizer.debug -input data\models\tr.dae -output data\models\tr.gek -smooth 75
+.\debug\optimizer.debug -input data\models\bl.dae -output data\models\bl.gek -smooth 75
+.\debug\optimizer.debug -input data\models\br.dae -output data\models\br.gek -smooth 75
 
 pause
