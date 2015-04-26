9e46da21ad1b0199a2a809f9f0f9efb9fb9b3cc1
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-04-26 21:35:21 +0000

    Adding tiled models

--- bin/models.bat
+++ bin/models.bat
@@ -2,10 +2,4 @@
 
 .\debug\optimizer.debug -input data\models\boulder.dae -output data\models\boulder.gek -smooth 75
 
-.\debug\optimizer.debug -input data\models\center.dae -output data\models\center.gek -smooth 75
-.\debug\optimizer.debug -input data\models\tl.dae -output data\models\tl.gek -smooth 75
-.\debug\optimizer.debug -input data\models\tr.dae -output data\models\tr.gek -smooth 75
-.\debug\optimizer.debug -input data\models\bl.dae -output data\models\bl.gek -smooth 75
-.\debug\optimizer.debug -input data\models\br.dae -output data\models\br.gek -smooth 75
-
 pause
