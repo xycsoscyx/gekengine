443e397240c50dcc8c38275ef1defedc8212b320
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-04-27 19:11:25 +0000

    Adding dungeon materials

--- bin/models.bat
+++ bin/models.bat
@@ -2,4 +2,9 @@
 
 .\debug\optimizer.debug -input data\models\boulder.dae -output data\models\boulder.gek -smooth 75
 
+For /R "data/models/dungeon" %%# in (*.dae) Do (
+    del "%%~dpn#.gek"
+    debug\optimizer.debug -input "%%~#" -output "%%~dpn#.gek" -smooth 75
+)
+
 pause
