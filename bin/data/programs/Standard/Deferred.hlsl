3aca58370adccd6b36169f76496874a8c785c292
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-06-05 08:32:10 +0000

    Updating shaders with new naming convention and using global defines instead of constant values

--- bin/data/programs/Standard/Deferred.hlsl
+++ bin/data/programs/Standard/Deferred.hlsl
@@ -12,30 +12,30 @@ struct OutputPixel
 
 OutputPixel mainPixelProgram(in InputPixel inputPixel)
 {
-    float4 nAlbedo = (Maps::albedo.Sample(Global::linearSampler, inputPixel.texcoord) * inputPixel.color);
+    float4 albedo = (Maps::albedo.Sample(Global::linearSampler, inputPixel.texcoord) * inputPixel.color);
 
     [branch]
-    if(nAlbedo.a < 0.5f)
+    if(albedo.a < 0.5f)
     {
         discard;
     }
 
     // Viewspace vertex normal
-    float3 nViewNormal = (normalize(inputPixel.viewnormal) * (inputPixel.frontface ? 1 : -1));
+    float3 viewNormal = (normalize(inputPixel.viewnormal) * (inputPixel.frontface ? 1 : -1));
                 
-    float3 nNormal;
+    float3 normal;
     // Normals stored as 3Dc format, so [0,1] XY components only
-    nNormal.xy = ((gs_pNormalMap.Sample(gs_pLinearSampler, inputPixel.texcoord).yx * 2.0) - 1.0);
-    nNormal.z = sqrt(1.0 - dot(nNormal.xy, nNormal.xy));
+    normal.xy = ((Material::Maps::normal.Sample(Global::linearSampler, inputPixel.texcoord).yx * 2.0) - 1.0);
+    normal.z = sqrt(1.0 - dot(normal.xy, normal.xy));
 
-    float3x3 nCoTangentFrame = GetCoTangentFrame(-inputPixel.viewposition.xyz, nViewNormal, inputPixel.texcoord);
-    nNormal = mul(nNormal, nCoTangentFrame);
+    float3x3 coTangentFrame = getCoTangentFrame(-inputPixel.viewposition.xyz, viewNormal, inputPixel.texcoord);
+    normal = mul(normal, coTangentFrame);
 
     OutputPixel outputPixel;
-    outputPixel.albedo.xyz = nAlbedo.xyz;
-    outputPixel.albedo.a   = 0;//gs_bMaterialFullBright;
-    outputPixel.normal     = EncodeNormal(nNormal);
+    outputPixel.albedo.xyz = albedo.xyz;
+    outputPixel.albedo.a   = Material::Properties::color;
+    outputPixel.normal     = encodeNormal(normal);
     outputPixel.depth      = (inputPixel.viewposition.z / Camera::maximumDistance);
-    outputPixel.info       = gs_pInfoMap.Sample(gs_pLinearSampler, inputPixel.texcoord);
+    outputPixel.info       = Material::Maps::info.Sample(Global::linearSampler, inputPixel.texcoord);
     return outputPixel;
 }
