ad3a99cfae9db82aad056a69cb3362160aa60d41
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2015-11-09 20:04:06 +0000

    Implementing simple IBL for ambient lighting

--- bin/data/programs/Standard/Ambient.hlsl
+++ bin/data/programs/Standard/Ambient.hlsl
@@ -19,15 +19,16 @@ float4 mainPixelProgram(in InputPixel inputPixel) : SV_TARGET0
         float pixelDepth = Resources::depthBuffer.Sample(Global::pointSampler, inputPixel.texcoord);
         float3 pixelPosition = getViewPosition(inputPixel.texcoord, pixelDepth);
         float3 pixelNormal = decodeNormal(Resources::normalBuffer.Sample(Global::pointSampler, inputPixel.texcoord));
-		return pixelNormal.xyzz;
 
         float3 viewNormal = -normalize(pixelPosition);
 
-        float3 lightColor = Resources::ambientLightMap.Sample(Global::pointSampler, pixelNormal);
-		return lightColor.xyzz;
+        float3x3 inverseViewMatrix = (float3x3)Camera::viewMatrix;
+        float3 lightNormal = -mul(inverseViewMatrix, pixelNormal);
+        float3 lightColor = Resources::ambientLightMap.Sample(Global::pointSampler, -lightNormal);
+
         float3 diffuseContribution = 0.0f;
         float3 specularContribution = 0.0f;
-        getBRDF(albedoTerm, pixelNormal, -pixelNormal, viewNormal, pixelInfo, diffuseContribution, specularContribution);
+        getBRDF(albedoTerm, pixelNormal, lightNormal, viewNormal, pixelInfo, diffuseContribution, specularContribution);
         lightingContribution = saturate(lightColor * diffuseContribution) + saturate(lightColor * specularContribution);
     }
 
