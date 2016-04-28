ad0784153469e0375ceb6a42e59b55926e55712c
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2016-04-28 17:49:04 +0000

    Fixing depth based weight in OIT and adding comments to paper about code

--- bin/data/programs/Transparent/Prepare.hlsl
+++ bin/data/programs/Transparent/Prepare.hlsl
@@ -3,6 +3,8 @@
 #include "GEKGlobal.hlsl"
 #include "GEKUtility.hlsl"
 
+// Weight Based OIT
+// http://jcgt.org/published/0002/02/09/paper.pdf
 OutputPixel mainPixelProgram(InputPixel inputPixel)
 {
     float sceneDepth = Resources::depthBuffer[inputPixel.position.xy];
@@ -12,10 +14,11 @@ OutputPixel mainPixelProgram(InputPixel inputPixel)
     float4 color = (Resources::albedo.Sample(Global::linearWrapSampler, inputPixel.texCoord) * inputPixel.color);
     color.a *= depthDelta;
 
-    float weight = max(min(1.0, max(max(color.r, color.g), color.b) * color.a), color.a) * clamp(0.03 / (1e-5 + pow(depth, 4.0)), 1e-2, 3e3);
+    float reveal = max(min(1.0, max(max(color.r, color.g), color.b) * color.a), color.a)
+        * clamp(0.03 / (1e-5 + pow(-depth, 4.0)), 1e-2, 3e3); // Eq 9
 
     OutputPixel outputPixel;
-    outputPixel.blendBuffer = (float4((color.rgb * color.a), color.a) * weight);
-    outputPixel.weightBuffer = color.a;
+    outputPixel.accumulationBuffer = (float4((color.rgb * color.a), color.a) * reveal);
+    outputPixel.revealBuffer = color.a;
     return outputPixel;
 }
--- bin/data/programs/Transparent/Present.hlsl
+++ bin/data/programs/Transparent/Present.hlsl
@@ -3,12 +3,14 @@
 #include "GEKGlobal.hlsl"
 #include "GEKUtility.hlsl"
 
+// Weight Based OIT
+// http://jcgt.org/published/0002/02/09/paper.pdf
 float4 mainPixelProgram(InputPixel inputPixel) : SV_TARGET0
 {
-    float4 blend = Resources::blendBuffer.Sample(Global::pointSampler, inputPixel.texCoord);
-    float weight = Resources::weightBuffer.Sample(Global::pointSampler, inputPixel.texCoord);
+    float4 accumulation = Resources::accumulationBuffer.Sample(Global::pointSampler, inputPixel.texCoord);
+    float reveal = Resources::revealBuffer.Sample(Global::pointSampler, inputPixel.texCoord);
 
-    float3 average = blend.rgb / max(blend.a, 0.00001);
-    return float4(average * (1 - weight), weight);
+    float3 average = (accumulation.rgb / max(accumulation.a, 1e-5));
+    return float4((average * (1.0 - reveal)), reveal);
 
 }
