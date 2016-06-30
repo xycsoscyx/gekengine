10a42db3b506800955f476326ebedbae2ea02ff3
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2016-06-30 16:35:27 +0000

    Cleaning up transparency pass, still doesn't darken particles over other particles, but does darken properly over scene

--- bin/data/programs/Transparent/Calculate.hlsl
+++ bin/data/programs/Transparent/Calculate.hlsl
@@ -5,7 +5,7 @@
 
 namespace Settings
 {
-    static const uint Equation = 9;
+    static const uint Equation = 10;
 };
 
 // Weight Based OIT
@@ -13,38 +13,40 @@ namespace Settings
 OutputPixel mainPixelProgram(InputPixel inputPixel)
 {
     float4 albedo = (Resources::albedo.Sample(Global::linearWrapSampler, inputPixel.texCoord) * inputPixel.color);
-    albedo.rgb *= albedo.a;
+    float reveal = albedo.a;
+    float weight = albedo.a;
 
     const float3 transmission = inputPixel.color1.rgb;
-    //albedo.a *= saturate(1.0 - dot(inputPixel.color1.rgb, (1.0 / 3.0)));
+    //weight *= saturate(1.0 - dot(inputPixel.color1.rgb, (1.0 / 3.0)));
 
     // Soften edges when transparent surfaces intersect solid surfaces
     const float sceneDepth = getLinearDepth(Resources::depthBuffer[inputPixel.position.xy]);
     const float depthDelta = saturate((sceneDepth - inputPixel.viewPosition.z) * 2.5);
-    //albedo.a *= depthDelta;
+    //weight *= depthDelta;
 
-    float reveal;
     switch (Settings::Equation)
     {
     case 7: // View Depth
-        reveal = rcp(Math::Epsilon + pow((inputPixel.viewPosition.z / 5.0), 2.0) + pow((inputPixel.viewPosition.z / 200.0), 6.0));
+        weight *= (10.0 / (Math::Epsilon + pow((inputPixel.viewPosition.z / 5.0), 2.0) + pow((inputPixel.viewPosition.z / 200.0), 6.0)));
         break;
 
     case 8: // View Depth
-        reveal = rcp(Math::Epsilon + pow((inputPixel.viewPosition.z / 10.0), 3.0) + pow((inputPixel.viewPosition.z / 200.0), 6.0));
+        weight *= (10.0 / (Math::Epsilon + pow((inputPixel.viewPosition.z / 10.0), 3.0) + pow((inputPixel.viewPosition.z / 200.0), 6.0)));
         break;
 
     case 9: // View Depth
-        reveal = (0.03 / (Math::Epsilon + pow((inputPixel.viewPosition.z / 200.0), 4.0)));
+        weight *= (0.03 / (Math::Epsilon + pow((inputPixel.viewPosition.z / 200.0), 4.0)));
         break;
 
     case 10: // Clip Depth
-        reveal = (30000 * pow((1.0 - inputPixel.position.z), 3.0));
+        weight *= dot(3e3, pow((1.0 - inputPixel.position.z), 3.0));
         break;
     };
 
+    weight = clamp(weight, 1e-2, 3e3);
+
     OutputPixel outputPixel;
-    outputPixel.accumulationBuffer = (albedo * reveal);
-    outputPixel.revealBuffer = albedo.a;
+    outputPixel.accumulationBuffer = float4((albedo.rgb * weight), weight);
+    outputPixel.revealBuffer = reveal;
     return outputPixel;
 }
