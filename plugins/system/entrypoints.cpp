2f9089e99a4915606515626ca9b3f531ec50901d
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2016-08-09 21:11:26 +0000

    Renaming system source files and adding shell for OpenGL cross platform video device

--- plugins/system/entrypoints.cpp
+++ plugins/system/entrypoints.cpp
@@ -17,9 +17,15 @@ namespace Gek
         GEK_DECLARE_CONTEXT_USER(Device);
     };
 
+    namespace OpenGL4_5
+    {
+        GEK_DECLARE_CONTEXT_USER(Device);
+    };
+
     GEK_CONTEXT_BEGIN(System);
         GEK_CONTEXT_ADD_CLASS(System::Input, DirectInput8::System);
         GEK_CONTEXT_ADD_CLASS(Device::Audio, DirectSound8::Device);
         GEK_CONTEXT_ADD_CLASS(Device::Video, Direct3D11::Device);
+        //GEK_CONTEXT_ADD_CLASS(Device::Video, OpenGL4_5::Device);
     GEK_CONTEXT_END();
 }; // namespace Gek
