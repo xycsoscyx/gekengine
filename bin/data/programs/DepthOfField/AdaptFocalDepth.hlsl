77736f54f23c7702cafd003f08fe3132750c8c13
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2016-08-12 03:14:25 +0000

    DoF filter working, need to fix issue when combining with tone mapping

--- bin/data/programs/DepthOfField/AdaptFocalDepth.hlsl
+++ bin/data/programs/DepthOfField/AdaptFocalDepth.hlsl
@@ -5,14 +5,14 @@
 
 namespace Defines
 {
-    static const float adaptionRate = 2.0;
+    static const float adaptionRate = 5.0;
 };
 
 [numthreads(1, 1, 1)]
 void mainComputeProgram(void)
 {
     float averageDepth = UnorderedAccess::averageFocalDepth[0];
-    float currentDepth = getLinearDepthFromSample(Resources::depthBuffer.Load(int3((Shader::targetSize *  0.5), 0)));
+    float currentDepth = getLinearDepthFromSample(Resources::depthBuffer[int2(Shader::targetSize * 0.5)]);
     averageDepth += (currentDepth - averageDepth) * (1.0 - exp(-Engine::frameTime * Defines::adaptionRate));
 
     UnorderedAccess::averageFocalDepth[0] = averageDepth;
--- bin/data/programs/DepthOfField/CalculateBlur.hlsl
+++ bin/data/programs/DepthOfField/CalculateBlur.hlsl
@@ -7,35 +7,36 @@ static const float width = Shader::targetSize.x;
 static const float height = Shader::targetSize.y;
 static const float2 texel = Shader::pixelSize;
 
-int samples = 3; //samples on the first ring
-int rings = 5; //ring count
+static const int sampleCount = 4;
+static const int ringCount = 7;
 
-float range = 4.0; //focal range
-float maxblur = 1.25; //clamp value of max blur
+static const float focalRange = 10.0;
 
-float threshold = 0.5; //highlight threshold;
-float gain = 10.0; //highlight gain;
+static const float highlightThreshold = 0.5;
+static const float highlightGain = 10.0;
 
-float bias = 0.4; //bokeh edge bias
-float fringe = 0.5; //bokeh chromatic aberration/fringing
+static const float bokehEdgeBias = 0.4;
 
-bool noise = true; //use noise instead of pattern for sample dithering
-float namount = 0.0001; //dither amount
+static const bool enableChromaticAberation = true;
+static const float bokehChromaticAberation = 0.5;
 
-bool depthblur = true; //blur the depth buffer?
-float dbsize = 2.0; //depthblursize
+static const bool enableNoise = true;
+static const float noiseAmount = 0.0001;
 
-/*
+static const bool enableDepthBlur = true;
+static const float depthBlurSize = 2.0;
+
+/* 
     next part is experimental
     not looking good with small sample and ring count
-    looks okay starting from samples = 4, rings = 4
-*/
-bool pentagon = false; //use pentagon as bokeh shape?
-float feather = 0.4; //pentagon shape feather
+    looks okay starting from sampleCount = 4, ringCount = 4
+ */
+static const bool enablePentagon = false;
+static const float pentagonFeathering = 0.4;
 
-float getPentagonalShape(float2 texCoord) //pentagonal shape
+float getPentagonalShape(float2 texCoord)
 {
-    static const float scale = float(rings) - 1.3;
+    static const float scale = float(ringCount) - 1.3;
     static const float4  HS0 = float4(1.0, 0.0, 0.0, 1.0);
     static const float4  HS1 = float4(0.309016994, 0.951056516, 0.0, 1.0);
     static const float4  HS2 = float4(-0.809016994, 0.587785252, 0.0, 1.0);
@@ -43,87 +44,78 @@ float getPentagonalShape(float2 texCoord) //pentagonal shape
     static const float4  HS4 = float4(0.309016994, -0.951056516, 0.0, 1.0);
     static const float4  HS5 = float4(0.0, 0.0, 1.0, 1.0);
 
-    static const float4  one = 1.0;
+    float4 texCoordScale = float4(texCoord, scale.xx);
+    float4 distance = float4(
+        dot(texCoordScale, HS0),
+        dot(texCoordScale, HS1),
+        dot(texCoordScale, HS2),
+        dot(texCoordScale, HS3));
+    distance = smoothstep(-pentagonFeathering, pentagonFeathering, distance);
 
-    float4 P = float4(texCoord, scale.xx);
+    float inOrOut = -4.0;
+    inOrOut += dot(distance, 1.0);
 
-    float4 dist = 0.0;
-    float inorout = -4.0;
+    distance.x = dot(texCoordScale, HS4);
+    distance.y = HS5.w - abs(texCoordScale.z);
+    distance = smoothstep(-pentagonFeathering, pentagonFeathering, distance);
 
-    dist.x = dot(P, HS0);
-    dist.y = dot(P, HS1);
-    dist.z = dot(P, HS2);
-    dist.w = dot(P, HS3);
-
-    dist = smoothstep(-feather, feather, dist);
-
-    inorout += dot(dist, one);
-
-    dist.x = dot(P, HS4);
-    dist.y = HS5.w - abs(P.z);
-
-    dist = smoothstep(-feather, feather, dist);
-    inorout += dist.x;
-
-    return clamp(inorout, 0.0, 1.0);
+    inOrOut += distance.x;
+    return clamp(inOrOut, 0.0, 1.0);
 }
 
-float getBlurredDepth(float2 texCoord) //blurring depth
+float getBlurredDepth(float2 texCoord) // blurring depth
 {
-    float d = 0.0;
-    float kernel[9];
-    float2 offset[9];
-
-    float2 wh = texel * dbsize;
-
-    offset[0] = float2(-wh.x, -wh.y);
-    offset[1] = float2(0.0, -wh.y);
-    offset[2] = float2(wh.x, -wh.y);
-
-    offset[3] = float2(-wh.x, 0.0);
-    offset[4] = float2(0.0, 0.0);
-    offset[5] = float2(wh.x, 0.0);
-
-    offset[6] = float2(-wh.x, wh.y);
-    offset[7] = float2(0.0, wh.y);
-    offset[8] = float2(wh.x, wh.y);
-
-    kernel[0] = 1.0 / 16.0;   kernel[1] = 2.0 / 16.0;   kernel[2] = 1.0 / 16.0;
-    kernel[3] = 2.0 / 16.0;   kernel[4] = 4.0 / 16.0;   kernel[5] = 2.0 / 16.0;
-    kernel[6] = 1.0 / 16.0;   kernel[7] = 2.0 / 16.0;   kernel[8] = 1.0 / 16.0;
-
-    for (int i = 0; i < 9; i++)
+    static const float2 texelOffset = (texel * depthBlurSize);
+    float2 offset[9] =
     {
-        float tmp = Resources::depthBuffer.SampleLevel(Global::pointSampler, texCoord + offset[i], 0).r;
-        d += tmp * kernel[i];
+        float2(-texelOffset.x, -texelOffset.y), 
+        float2(0.0, -texelOffset.y), 
+        float2(texelOffset.x, -texelOffset.y), 
+        float2(-texelOffset.x, 0.0), 
+        float2(0.0, 0.0), 
+        float2(texelOffset.x, 0.0), 
+        float2(-texelOffset.x, texelOffset.y), 
+        float2(0.0, texelOffset.y), 
+        float2(texelOffset.x, texelOffset.y), 
+    };
+
+    float kernel[9] = 
+    {
+        (1.0 / 16.0), (2.0 / 16.0), (1.0 / 16.0),
+        (2.0 / 16.0), (4.0 / 16.0), (2.0 / 16.0),
+        (1.0 / 16.0), (2.0 / 16.0), (1.0 / 16.0),
+    };
+
+    float blurredDepth = 0.0;
+    for (int index = 0; index < 9; index++)
+    {
+        blurredDepth += getLinearDepthFromSample(Resources::depthBuffer.SampleLevel(Global::pointSampler, texCoord + offset[index], 0)) * kernel[index];
     }
 
-    return d;
+    return blurredDepth;
 }
 
-
-float3 color(float2 texCoord, float blur) //processing the sample
+float3 getFringedColor(float2 texCoord, float blur)
 {
-    float3 col = 0.0;
+    float3 fringedColor = (enableChromaticAberation ? float3(
+        Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(0.0, 1.0) * texel * bokehChromaticAberation * blur, 0).r,
+        Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(-0.866, -0.5) * texel * bokehChromaticAberation * blur, 0).g,
+        Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(0.866, -0.5) * texel * bokehChromaticAberation * blur, 0).b) :
+        Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + texel * blur, 0));
 
-    col.r = Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(0.0, 1.0)*texel*fringe*blur, 0).r;
-    col.g = Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(-0.866, -0.5)*texel*fringe*blur, 0).g;
-    col.b = Resources::finalCopy.SampleLevel(Global::pointSampler, texCoord + float2(0.866, -0.5)*texel*fringe*blur, 0).b;
-
-    static const float3 lumcoeff = float3(0.299, 0.587, 0.114);
-    float lum = dot(col.rgb, lumcoeff);
-    float thresh = max((lum - threshold)*gain, 0.0);
-    return col + lerp(0.0, col, thresh*blur);
+    float luminance = getLuminance(fringedColor);
+    float thresh = (saturate(luminance - highlightThreshold) * highlightGain);
+    return fringedColor + lerp(0.0, fringedColor, thresh * blur);
 }
 
-float2 rand(in float2 coord) //generating noise/pattern texture for dithering
+float2 getNoise(in float2 coord)
 {
-    float noiseX = ((frac(1.0 - coord.x*(width / 2.0))*0.25) + (frac(coord.y*(height / 2.0))*0.75))*2.0 - 1.0;
-    float noiseY = ((frac(1.0 - coord.x*(width / 2.0))*0.75) + (frac(coord.y*(height / 2.0))*0.25))*2.0 - 1.0;
-    if (noise)
+    float noiseX = ((frac(1.0 - coord.x * (width / 2.0)) * 0.25) + (frac(coord.y * (height / 2.0)) * 0.75)) * 2.0 - 1.0;
+    float noiseY = ((frac(1.0 - coord.x * (width / 2.0)) * 0.75) + (frac(coord.y * (height / 2.0)) * 0.25)) * 2.0 - 1.0;
+    if (enableNoise)
     {
-        noiseX = clamp(frac(sin(dot(coord, float2(12.9898, 78.233))) * 43758.5453), 0.0, 1.0)*2.0 - 1.0;
-        noiseY = clamp(frac(sin(dot(coord, float2(12.9898, 78.233)*2.0)) * 43758.5453), 0.0, 1.0)*2.0 - 1.0;
+        noiseX = clamp(frac(sin(dot(coord, float2(12.9898, 78.233))) * 43758.5453), 0.0, 1.0) * 2.0 - 1.0;
+        noiseY = clamp(frac(sin(dot(coord, float2(12.9898, 78.233) * 2.0)) * 43758.5453), 0.0, 1.0) * 2.0 - 1.0;
     }
 
     return float2(noiseX, noiseY);
@@ -131,40 +123,32 @@ float2 rand(in float2 coord) //generating noise/pattern texture for dithering
 
 float3 mainPixelProgram(InputPixel inputPixel) : SV_TARGET0
 {
-    float depth = Resources::depthBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord, 0).x;
-    if (depthblur)
-    {
-        depth = getBlurredDepth(inputPixel.texCoord);
-    }
-
     float focalDepth = Resources::averageFocalDepth[0];
-    float blur = clamp((abs(depth - focalDepth) / range)*100.0, -maxblur, maxblur);
-    float2 noise = rand(inputPixel.texCoord)*namount*blur;
+    float sceneDepth = (enableDepthBlur ? getBlurredDepth(inputPixel.texCoord) : getLinearDepthFromSample(Resources::depthBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord, 0)));
+    float blurDistance = saturate(abs(sceneDepth - focalDepth) / focalRange);
 
-    float noiseWidth = (1.0 / width)*blur + noise.x;
-    float noiseHeight = (1.0 / height)*blur + noise.y;
+    float2 noise = getNoise(inputPixel.texCoord) * noiseAmount * blurDistance;
+    noise = texel * blurDistance + noise;
 
-    float3 col = Resources::finalCopy.SampleLevel(Global::pointSampler, inputPixel.texCoord, 0);
-    float s = 1.0;
+    float3 finalColor = Resources::finalCopy.SampleLevel(Global::pointSampler, inputPixel.texCoord, 0);
+    float totalWeight = 1.0;
 
-    for (float i = 1.0; i <= rings; i++)
+    for (float ring = 1.0; ring <= ringCount; ring++)
     {
-        float ringsamples = i * float(samples);
-        for (float j = 0.0; j < ringsamples; j++)
+        float ringsamples = ring * float(sampleCount);
+        float step = Math::Tau / float(ringsamples);
+        for (float tap = 0.0; tap < ringsamples; tap++)
         {
-            float step = Math::Tau / float(ringsamples);
-            float pw = (cos(j*step)*i);
-            float ph = (sin(j*step)*i);
-            float p = 1.0;
-            if (pentagon)
-            {
-                p = getPentagonalShape(float2(pw, ph));
-            }
+            float2 offset = float2(
+                (cos(tap * step) * ring), 
+                (sin(tap * step) * ring));
 
-            col += color(inputPixel.texCoord + float2(pw*noiseWidth, ph*noiseHeight), blur)*lerp(1.0, (i / float(rings)), bias)*p;
-            s += 1.0*lerp(1.0, (i / float(rings)), bias)*p;
+            float pentagon = (enablePentagon ? getPentagonalShape(offset) : 1.0);
+            float weight = (lerp(1.0, (ring / float(ringCount)), bokehEdgeBias) * pentagon);
+            finalColor += (getFringedColor(inputPixel.texCoord + (offset * noise), blurDistance) * weight);
+            totalWeight += weight;
         }
     }
 
-    return (col / s);
+    return (finalColor / totalWeight);
 }
