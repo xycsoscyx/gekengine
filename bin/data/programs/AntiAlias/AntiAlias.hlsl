df57d33746840ccf48d46f87bfceb9fa4769a666
gekengine <gekengine@25051c08-226a-4b37-825a-0e70ef16a3dd>
2016-08-19 15:17:52 +0000

    Cleaning up FXAA code

--- bin/data/programs/AntiAlias/AntiAlias.hlsl
+++ bin/data/programs/AntiAlias/AntiAlias.hlsl
@@ -3,13 +3,6 @@
 #include "GEKGlobal.hlsl"
 #include "GEKUtility.hlsl"
 
-namespace Defines
-{
-    static const float FXAA_REDUCE_MIN = (1.0 / 128.0);
-    static const float FXAA_REDUCE_MUL = (1.0 / 8.0);
-    static const float FXAA_SPAN_MAX = 8.0;
-};
-
 // https://github.com/mattdesl/glsl-fxaa
 //optimized version for mobile, where dependent 
 //texture reads can be a bottleneck
@@ -40,17 +33,18 @@ float3 mainPixelProgram(InputPixel inputPixel) : SV_TARGET0
     float2 dir;
     dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
     dir.y = ((lumaNW + lumaSW) - (lumaNE + lumaSE));
-    float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * Defines::FXAA_REDUCE_MUL), Defines::FXAA_REDUCE_MIN);
+    float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * Defines::reduceMultiplier), Defines::reduceMinimum);
 
     float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
-    dir = min(Defines::FXAA_SPAN_MAX, max(-Defines::FXAA_SPAN_MAX, dir * rcpDirMin)) * Shader::pixelSize;
+    dir = min(Defines::spanMaximum, max(-Defines::spanMaximum, dir * rcpDirMin)) * Shader::pixelSize;
 
-    float3 rgbA = 0.5 * (
-        Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * (1.0 / 3.0 - 0.5), 0) +
-        Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * (2.0 / 3.0 - 0.5), 0));
-    float3 rgbB = rgbA * 0.5 + 0.25 * (
-        Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * -0.5, 0) +
-        Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * 0.5, 0));
+    float3 rgbA = Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * (1.0 / 3.0 - 0.5), 0);
+    rgbA += Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * (2.0 / 3.0 - 0.5), 0);
+    rgbA *= 0.5;
+
+    float3 rgbB = Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * -0.5, 0);
+    rgbB += Resources::screenBuffer.SampleLevel(Global::pointSampler, inputPixel.texCoord + dir * 0.5, 0);
+    rgbB = ((rgbA * 0.5) + (0.25 * rgbB));
 
     float lumaB = getLuminance(rgbB);
     if ((lumaB < lumaMin) || (lumaB > lumaMax))
