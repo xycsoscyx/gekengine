d9a76e4dd6671232f166576e3467998ded9dfe7e
Todd Zupan <toddzupan@gmail.com>
2016-11-28 20:51:27 -0800

    Moving final piece of lighting equation to global shader for all passes to use

--- bin/data/programs/Deferred/AccumulateLighting.hlsl
+++ bin/data/programs/Deferred/AccumulateLighting.hlsl
@@ -15,63 +15,7 @@ float3 mainPixelProgram(InputPixel inputPixel) : SV_TARGET0
     float3 surfacePosition = getPositionFromSample(inputPixel.texCoord, surfaceDepth);
     float3 surfaceNormal = getDecodedNormal(Resources::normalBuffer[inputPixel.screen.xy]);
 
-    float3 viewDirection = -normalize(surfacePosition);
-    float3 reflectNormal = reflect(-viewDirection, surfaceNormal);
-
-    float VdotN = saturate(dot(viewDirection, surfaceNormal));
-
-    // materialAlpha modifications by Disney - s2012_pbs_disney_brdf_notes_v2.pdf
-    float materialAlpha = pow(materialRoughness, 2.0);
-
-    // reduce roughness range from [0 .. 1] to [0.5 .. 1]
-    float materialDisneyAlpha = pow(0.5 + materialRoughness * 0.5, 2.0);
-
-	float3 surfaceIrradiance = 0.0;
-
-    for (uint directionalIndex = 0; directionalIndex < Lights::directionalCount; directionalIndex++)
-    {
-		float3 lightDirection = Lights::directionalList[directionalIndex].direction;
-		float3 lightRadiance = Lights::directionalList[directionalIndex].radiance;
-
-		surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
-	}
-
-	uint clusterOffset = getClusterOffset(inputPixel.screen.xy, surfacePosition.z);
-	uint3 clusterData = Lights::clusterDataList[clusterOffset];
-	uint indexOffset = clusterData.x;
-	uint pointLightCount = clusterData.y;
-	uint spotLightCount = clusterData.z;
-
-	while (pointLightCount-- > 0)
-	{
-		uint lightIndex = Lights::clusterIndexList[indexOffset++];
-		Lights::PointData lightData = Lights::pointList[lightIndex];
-
-		float3 lightRay = (lightData.position - surfacePosition);
-		float3 centerToRay = (lightRay - (dot(lightRay, reflectNormal) * reflectNormal));
-		float3 closestPoint = (lightRay + (centerToRay * saturate(lightData.radius / length(centerToRay))));
-		float lightDistance = length(closestPoint);
-		float3 lightDirection = normalize(closestPoint);
-		float3 lightRadiance = (lightData.radiance * getFalloff(lightDistance, lightData.range));
-
-        surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
-	};
-
-	while (spotLightCount-- > 0)
-	{
-		uint lightIndex = Lights::clusterIndexList[indexOffset++];
-		Lights::SpotData lightData = Lights::spotList[lightIndex];
-
-		float3 lightRay = (lightData.position - surfacePosition);
-		float lightDistance = length(lightRay);
-		float3 lightDirection = (lightRay / lightDistance);
-		float rho = saturate(dot(lightData.direction, -lightDirection));
-		float spotFactor = pow(saturate(rho - lightData.outerAngle) / (lightData.innerAngle - lightData.outerAngle), lightData.coneFalloff);
-		float3 lightRadiance = (lightData.radiance * getFalloff(lightDistance, lightData.range) * spotFactor);
-
-		surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
-	};
-
+    float3 surfaceIrradiance = getSurfaceIrradiance(inputPixel.screen.xy, surfacePosition, surfaceNormal, materialAlbedo, materialRoughness, materialMetallic);
 	float surfaceAmbient = Resources::ambientBuffer[inputPixel.screen.xy];
 	return (surfaceIrradiance + (materialAlbedo * surfaceAmbient * 0.005));
 }
--- bin/data/programs/GEKLighting.hlsl
+++ bin/data/programs/GEKLighting.hlsl
@@ -124,3 +124,65 @@ uint getClusterOffset(float2 screenPosition, float surfaceDepth)
 
     return ((((gridSlice * Lights::gridSize.y) + gridLocation.y) * Lights::gridSize.x) + gridLocation.x);
 }
+
+float3 getSurfaceIrradiance(float2 screenCoord, float3 surfacePosition, float3 surfaceNormal, float3 materialAlbedo, float materialRoughness, float materialMetallic)
+{
+    float3 viewDirection = -normalize(surfacePosition);
+    float3 reflectNormal = reflect(-viewDirection, surfaceNormal);
+
+    float VdotN = saturate(dot(viewDirection, surfaceNormal));
+
+    // materialAlpha modifications by Disney - s2012_pbs_disney_brdf_notes_v2.pdf
+    float materialAlpha = pow(materialRoughness, 2.0);
+
+    // reduce roughness range from [0 .. 1] to [0.5 .. 1]
+    float materialDisneyAlpha = pow(0.5 + materialRoughness * 0.5, 2.0);
+
+    float3 surfaceIrradiance = 0.0;
+
+    for (uint directionalIndex = 0; directionalIndex < Lights::directionalCount; directionalIndex++)
+    {
+        float3 lightDirection = Lights::directionalList[directionalIndex].direction;
+        float3 lightRadiance = Lights::directionalList[directionalIndex].radiance;
+
+        surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
+    }
+
+    uint clusterOffset = getClusterOffset(screenCoord, surfacePosition.z);
+    uint3 clusterData = Lights::clusterDataList[clusterOffset];
+    uint indexOffset = clusterData.x;
+    uint pointLightCount = clusterData.y;
+    uint spotLightCount = clusterData.z;
+
+    while (pointLightCount-- > 0)
+    {
+        uint lightIndex = Lights::clusterIndexList[indexOffset++];
+        Lights::PointData lightData = Lights::pointList[lightIndex];
+
+        float3 lightRay = (lightData.position - surfacePosition);
+        float3 centerToRay = (lightRay - (dot(lightRay, reflectNormal) * reflectNormal));
+        float3 closestPoint = (lightRay + (centerToRay * saturate(lightData.radius / length(centerToRay))));
+        float lightDistance = length(closestPoint);
+        float3 lightDirection = normalize(closestPoint);
+        float3 lightRadiance = (lightData.radiance * getFalloff(lightDistance, lightData.range));
+
+        surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
+    };
+
+    while (spotLightCount-- > 0)
+    {
+        uint lightIndex = Lights::clusterIndexList[indexOffset++];
+        Lights::SpotData lightData = Lights::spotList[lightIndex];
+
+        float3 lightRay = (lightData.position - surfacePosition);
+        float lightDistance = length(lightRay);
+        float3 lightDirection = (lightRay / lightDistance);
+        float rho = saturate(dot(lightData.direction, -lightDirection));
+        float spotFactor = pow(saturate(rho - lightData.outerAngle) / (lightData.innerAngle - lightData.outerAngle), lightData.coneFalloff);
+        float3 lightRadiance = (lightData.radiance * getFalloff(lightDistance, lightData.range) * spotFactor);
+
+        surfaceIrradiance += getSurfaceIrradiance(surfaceNormal, viewDirection, VdotN, materialAlbedo, materialRoughness, materialMetallic, materialAlpha, materialDisneyAlpha, lightDirection, lightRadiance);
+    };
+
+    return surfaceIrradiance;
+}
