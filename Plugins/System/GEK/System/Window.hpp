c61a4e1bd340e0c6322abbb9c76a52ca9b8e2383
Todd Zupan <toddzupan@gmail.com>
2016-12-12 13:59:52 -0800

    Planning out best way to implement window procedure for events

--- Plugins/System/GEK/System/Window.hpp
+++ Plugins/System/GEK/System/Window.hpp
@@ -9,11 +9,44 @@
 
 #include "GEK/Utility/Exceptions.hpp"
 #include "GEK/Utility/Context.hpp"
+#include <nano_signal_slot.hpp>
 
 namespace Gek
 {
     GEK_INTERFACE(Window)
     {
+        struct Event
+        {
+            struct Result
+            {
+                bool handled = false;
+                int32_t result;
+
+                Result(void)
+                {
+                }
+
+                Result(int32_t result)
+                    : handled(true)
+                    , result(result)
+                {
+                }
+            };
+
+            uint32_t message;
+            uint32_t unsignedParameter;
+            int32_t signedParameter;
+
+            Event(uint32_t message, uint32_t unsignedParameter, int32_t signedParameter)
+                : message(message)
+                , unsignedParameter(unsignedParameter)
+                , signedParameter(signedParameter)
+            {
+            }
+        };
+
         virtual ~Window(void) = default;
+
+        std::function<Event::Result(const Event &eventData)> onEvent;
     };
 }; // namespace Gek
--- Plugins/System/Win32Window.cpp
+++ Plugins/System/Win32Window.cpp
@@ -16,10 +16,62 @@ namespace Gek
             Window(Context *context)
                 : ContextRegistration(context)
             {
+                WNDCLASS windowClass;
+                windowClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+                windowClass.lpfnWndProc = [](HWND handle, uint32_t message, WPARAM wParam, LPARAM lParam) -> LRESULT
+                {
+                    Gek::Window *window = reinterpret_cast<Gek::Window *>(GetWindowLongPtr(handle, GWLP_USERDATA));
+                    if (window)
+                    {
+                        Gek::Window::Event eventData(message, wParam, lParam);
+                        auto resultValue = window->onEvent(eventData);
+                        if (resultValue.handled)
+                        {
+                            return resultValue.result;
+                        }
+                    }
+
+                    return DefWindowProc(handle, message, wParam, lParam);
+                };
+
+                windowClass.cbClsExtra = 0;
+                windowClass.cbWndExtra = 0;
+                windowClass.hInstance = GetModuleHandle(nullptr);
+                windowClass.hIcon = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(103));
+                windowClass.hCursor = LoadCursor(GetModuleHandle(nullptr), IDC_ARROW);
+                windowClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+                windowClass.lpszMenuName = nullptr;
+                windowClass.lpszClassName = L"GEKvX_Engine_Demo";
+                ATOM classAtom = RegisterClass(&windowClass);
+                if (!classAtom)
+                {
+                    throw std::exception("Unable to register window class");
+                }
+
+                auto windowFlags = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+
+                RECT clientRectangle;
+                clientRectangle.left = 0;
+                clientRectangle.top = 0;
+                clientRectangle.right = 1;
+                clientRectangle.bottom = 1;
+                AdjustWindowRect(&clientRectangle, WS_OVERLAPPEDWINDOW, false);
+                int windowWidth = (clientRectangle.right - clientRectangle.left);
+                int windowHeight = (clientRectangle.bottom - clientRectangle.top);
+                window = CreateWindow(windowClass.lpszClassName, L"GEKvX Application - Demo", windowFlags, CW_USEDEFAULT, CW_USEDEFAULT, windowWidth, windowHeight, nullptr, nullptr, GetModuleHandle(nullptr), nullptr);
+                if (window == nullptr)
+                {
+                    throw std::exception("Unable to create window");
+                }
             }
 
             ~Window(void)
             {
+                if (window)
+                {
+                    SetWindowLongPtr(window, GWLP_USERDATA, 0);
+                    DestroyWindow(window);
+                }
             }
         };
 
