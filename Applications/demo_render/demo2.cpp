ac75d05a7b904b8951a1e2b94d59a0bed2c3b195
Todd Zupan <toddzupan@gmail.com>
2017-02-08 08:19:28 -0800

    Adding render system demo

---
+++ Applications/demo_render/demo2.cpp
@@ -0,0 +1,39 @@
+﻿#include "GEK/Utility/Exceptions.hpp"
+#include "GEK/Utility/FileSystem.hpp"
+#include "GEK/Utility/String.hpp"
+#include "GEK/Utility/Context.hpp"
+#include "GEK/Utility/ContextUser.hpp"
+#include "GEK/Engine/Core.hpp"
+#include <experimental\filesystem>
+
+using namespace Gek;
+
+int CALLBACK wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ wchar_t *strCommandLine, _In_ int nCmdShow)
+{
+    try
+    {
+        auto pluginPath(FileSystem::GetModuleFilePath().getParentPath());
+        std::vector<FileSystem::Path> searchPathList;
+        searchPathList.push_back(pluginPath);
+
+        auto rootPath(pluginPath.getParentPath());
+        ContextPtr context(Context::Create(rootPath, searchPathList));
+        if (true)
+        {
+            Plugin::CorePtr core(context->createClass<Plugin::Core>(L"Engine::Core", (Window *)nullptr));
+            while (core->update())
+            {
+            };
+        }
+    }
+    catch (const std::exception &exception)
+    {
+        MessageBoxA(nullptr, StringUTF8::Format("Caught: %v\r\nType: %v", exception.what(), typeid(exception).name()), "GEK Engine - Error", MB_OK | MB_ICONERROR);
+    }
+    catch (...)
+    {
+        MessageBox(nullptr, L"Caught: Non-standard exception", L"GEK Engine - Error", MB_OK | MB_ICONERROR);
+    };
+
+    return 0;
+}
