fc2679b49c5fe1f98951d37081beeee295c186b4
Todd Zupan <toddzupan@gmail.com>
2017-05-30 16:38:19 -0700

    Updating models script

--- bin/models.bat
+++ bin/models.bat
@@ -1,14 +1,14 @@
-.\debug\createmodel -input data\models\rendering\buddha.obj -output data\models\buddha.gek -forcematerial demo/glass/glass_diffuse
-.\debug\createmodel -input data\models\rendering\mitsuba.obj -output data\models\mitsuba.gek -forcematerial demo/glass/glass_diffuse
-.\debug\createmodel -input data\models\rendering\dragon.obj -output data\models\dragon.gek -forcematerial demo/glass/glass_diffuse
-.\debug\createmodel -input data\models\rendering\teapot.obj -output data\models\teapot.gek -forcematerial demo/glass/glass_diffuse
-.\debug\createmodel -input data\models\rendering\testObj.obj -output data\models\testObj.gek -forcematerial demo/glass/glass_diffuse
+rem .\debug\createmodel -input data\models\rendering\buddha.obj -output data\models\buddha.gek -forcematerial demo/glass/glass_diffuse
+rem .\debug\createmodel -input data\models\rendering\mitsuba.obj -output data\models\mitsuba.gek -forcematerial demo/glass/glass_diffuse
+rem .\debug\createmodel -input data\models\rendering\dragon.obj -output data\models\dragon.gek -forcematerial demo/glass/glass_diffuse
+rem .\debug\createmodel -input data\models\rendering\teapot.obj -output data\models\teapot.gek -forcematerial demo/glass/glass_diffuse
+rem .\debug\createmodel -input data\models\rendering\testObj.obj -output data\models\testObj.gek -forcematerial demo/glass/glass_diffuse
+rem pause
+
+.\debug\createmodel -input data\models\rendering\sponza.fbx -output data\models\sponza.gek -flipCoords -smoothAngle:75 -unitsinfoot:13
 pause
 
-.\debug\createmodel -input data\models\rendering\sponza.fbx -output data\models\sponza.gek -flipCoords -smoothAngle:75
-pause
-
-.\debug\createtree -input data\models\physics\sponza.fbx -output data\models\sponza.bin
+.\debug\createtree -input data\models\physics\sponza.fbx -output data\models\sponza.bin -unitsinfoot:13
 pause
 
 .\debug\createmodel -input data\models\rendering\cube.fbx -output data\models\cube.gek -flipCoords
