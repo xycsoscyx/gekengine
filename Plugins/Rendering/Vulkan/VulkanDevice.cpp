ee94385b3a8c0953f9b3771191b13eb2aa5f7412
Todd Zupan <toddzupan@gmail.com>
2026-05-13 22:05:37 -0700

    Update VulkanDevice.cpp

--- Plugins/Rendering/Vulkan/VulkanDevice.cpp
+++ Plugins/Rendering/Vulkan/VulkanDevice.cpp
@@ -4135,9 +4135,7 @@ namespace Gek
                 VkAttachmentDescription colorAttachment{};
                 colorAttachment.format = swapChainImageFormat;
                 colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-                // Backbuffer should start from a known value each frame.
-                // Scene composition or UI can then render on top deterministically.
-                colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+                colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
                 colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
                 colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
                 colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
