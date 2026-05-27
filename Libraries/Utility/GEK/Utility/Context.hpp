e668cb8b4de3da19a693a57d5310bb0a1d515663
Todd Zupan <toddzupan@gmail.com>
2026-05-27 09:28:52 -0700

    Update Context.hpp

--- Libraries/Utility/GEK/Utility/Context.hpp
+++ Libraries/Utility/GEK/Utility/Context.hpp
@@ -103,6 +103,7 @@ namespace Gek
             auto derivedClass = dynamic_cast<TYPE *>(baseClass.get());
             if (!derivedClass)
             {
+                log(Error, "foobar");
                 log(Error, "Unable to cast {} from base context user to requested class of {}", className, typeid(TYPE).name());
                 return nullptr;
             }
